591df0ca7c46f81f67e522bd2b89b172bcee9204
Tinchev <st8tinchev@gmail.com>
2022-02-14 23:33:48 +0200

    Usage of mailtrap

--- static/js/home.js
+++ static/js/home.js
@@ -1,51 +1,59 @@
 function sendEmailOK() {
     Email.send({
-        SecureToken: "25a7ebfb-7a4f-48d0-879c-05ad60b9cca9",
-        To: 'st8tinchev@gmail.com',
-        From: "st2tinchev@gmail.com",
+        Host: "smtp.mailtrap.io",
+        Username: "20eea2d28171a3",
+        Password: "2c7d7d0171b54e",
+        Port: "465",
+        To: 'tozi@gmail.com',
+        From: "tozi@gmail.com",
         Subject: "Sending Email using javascript",
         Body: "Clicked OK",
     }).then(function (message) {
-        alert(message)
         location.href = "popUp8.html"
     });
 }
 
 function sendEmailMoreOK() {
     Email.send({
-        SecureToken: "25a7ebfb-7a4f-48d0-879c-05ad60b9cca9",
-        To: 'st8tinchev@gmail.com',
-        From: "st2tinchev@gmail.com",
+        Host: "smtp.mailtrap.io",
+        Username: "20eea2d28171a3",
+        Password: "2c7d7d0171b54e",
+        Port: "465",
+        To: 'tozi@gmail.com',
+        From: "tozi@gmail.com",
         Subject: "Sending Email using javascript",
         Body: "Clicked more OK",
     }).then(function (message) {
-        alert(message)
         location.href = "popUp8.html"
     });
 }
 
 function sendEmailYes() {
     Email.send({
-        SecureToken: "25a7ebfb-7a4f-48d0-879c-05ad60b9cca9",
-        To: 'st8tinchev@gmail.com',
-        From: "st2tinchev@gmail.com",
+        Host: "smtp.mailtrap.io",
+        Username: "20eea2d28171a3",
+        Password: "2c7d7d0171b54e",
+        Port: "465",
+        To: 'tozi@gmail.com',
+        From: "tozi@gmail.com",
         Subject: "Sending Email using javascript",
         Body: "Clicked yessss",
     }).then(function (message) {
-        alert(message)
         location.href = "popUp8.html"
     });
 }
 
 function sendEmailProbably() {
     Email.send({
-        SecureToken: "25a7ebfb-7a4f-48d0-879c-05ad60b9cca9",
-        To: 'st8tinchev@gmail.com',
-        From: "st2tinchev@gmail.com",
+        Host: "smtp.mailtrap.io",
+        Username: "20eea2d28171a3",
+        Password: "2c7d7d0171b54e",
+        Port: "465",
+        To: 'tozi@gmail.com',
+        From: "tozi@gmail.com",
         Subject: "Sending Email using javascript",
         Body: "Clicked probably",
     }).then(function (message) {
-        alert(message)
         location.href = "popUp8.html"
     });
 }
